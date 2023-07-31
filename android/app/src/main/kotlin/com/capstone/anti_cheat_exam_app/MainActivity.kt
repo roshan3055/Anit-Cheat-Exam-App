b1eb391acd762339670418bc23760446da9e8923
roshan3055 <81009695+roshan3055@users.noreply.github.com>
2023-08-01 01:17:32 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/capstone/anti_cheat_exam_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/capstone/anti_cheat_exam_app/MainActivity.kt
@@ -2,5 +2,5 @@ package com.capstone.anti_cheat_exam_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity() {  
 }
